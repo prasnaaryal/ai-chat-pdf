ed740b0698136347f02b46dfcdee2b499e2bb31f
Prasna Aryal <prasna101@gmail.com>
2024-05-26 11:27:20 +0545

    Revert "pinecone db added"
    This reverts commit 374198650644faaeebac8c89c0ac67a3695df22e.

--- src/components/ui/FileUpload.tsx
+++ src/components/ui/FileUpload.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { uploadToS3 } from "@/lib/s3";
+import { uploadToS3 } from "@/lib/db/s3";
 import { useMutation,UseMutationResult } from "@tanstack/react-query";
 import { Inbox, Loader2 } from "lucide-react";
 import React from "react";
@@ -48,10 +48,10 @@ const FileUpload = () => {
         }
         mutate(data, {
           onSuccess: (data) => {
-            toast.success(data.message);
+            console.log(data);
           },
           onError: (err) => {
-            toast.error("Error creating chat")
+            toast.error("Something went wrong")
           },
         });
       } catch (error) {
@@ -75,9 +75,6 @@ const FileUpload = () => {
         <>
         {/* loading state */}
         <Loader2 className="h-10 w-10 text-blue-500 animate-spin"/>
-        <p className="mt-2 text-sm text-slate-400">
-          Spilling tea to GPT
-        </p>
 
         </>):(
           <>

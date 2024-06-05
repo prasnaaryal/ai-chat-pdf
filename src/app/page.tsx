6c42fdee83e41bf1fc84d22b4efa08857549f8ca
prasnaaryal <prasna101@gmail.com>
2024-06-05 13:04:39 +0545

    added upload file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,20 @@
+import UploadFile from "@/components/UploadFile";
 import { Button } from "@/components/ui/button";
 import { UserButton, auth } from "@clerk/nextjs";
 import { LogIn } from "lucide-react";
 import Link from "next/link";
+import { useState } from "react";
 
 export default async function Home() {
   const { userId } = await auth();
   const isAuth = !!userId;
+
   return (
     <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <div className="flex flex-col items-center text-center">
-          <div className="flex items-center">
-            <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
+        <div className="flex flex-col gap-4 items-center text-center">
+          <div className="flex gap-4 items-center">
+            <h1 className="text-5xl font-semibold">Chat with any PDF</h1>
             <UserButton afterSignOutUrl="/" />
           </div>
 
@@ -26,12 +29,12 @@ export default async function Home() {
 
           <div className="w-full mt-4">
             {isAuth ? (
-              <h1></h1>
+              <UploadFile />
             ) : (
               <Link href="/sign-in">
                 <Button>
                   Login to get Started!
-                  <LogIn className="w-4 h-4 ml-2"/>
+                  <LogIn className="w-4 h-4 ml-2" />
                 </Button>
               </Link>
             )}
